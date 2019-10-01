cd701df5d92ebb3e4025e89284e53113bd9a273b
Diogo <drd.rclrd@gmail.com>
2019-10-01 10:36:48 -0300

    change code to file routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello' })
-})
+app.use(express.json());
 
 app.listen(3333);
