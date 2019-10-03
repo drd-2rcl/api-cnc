b795e9018cc738837bd3f4a305c28175f7cccca1
Diogo <drd.rclrd@gmail.com>
2019-10-02 22:51:37 -0300

    connected server and create validations for this

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
-
 const app = express();
+
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+  
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', err => {
+  console.log(`Mongoose default connection has occured \n${err}`);
+});
+
+db.on('disconnected', () => {
+  console.log('Mongoose default connection is disconnected');
+});
 
 app.use(express.json());
 app.use(routes);
